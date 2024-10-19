796bb37685c3578842085589926903948d5b77a9
Joshua Blais <josh@joshblais.com>
2024-10-19 08:05:32 -0600

    first public commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Doom Emacs Configuration
+
+This is a working configuration I have for any emacs use in the future.
---
+++ custom.el
@@ -0,0 +1,36 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" "7ea883b13485f175d3075c72fceab701b5bf76b2076f024da50dff4107d0db25" default))
+ '(elfeed-feeds
+   '(("https://joshblais.com/index.xml" blogs)
+     ("https://www.reddit.com/r/linux.rss" Reddit Linux)
+     ("https://www.reddit.com/r/unixporn.rss" Reddit)
+    ))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("/mnt/TrueNAS/org/Personal/birthdays.org" "/mnt/TrueNAS/org/projects.org" "/mnt/TrueNAS/org/Habits.org" "/mnt/TrueNAS/org/todo.org" "/mnt/TrueNAS/org/dates.org" "/mnt/TrueNAS/org/Revere/closings.org" "/mnt/TrueNAS/org/journal.org"))
+ '(package-selected-packages
+   '(templ-ts-mode multi-vterm protobuf-mode jabber elpher org-roam-ui dart-mode lsp-mode lsp-dart lsp-treemacs flycheck company lsp-ui company hover))
+ '(warning-suppress-types
+   '(((copilot copilot-no-mode-indent))
+     ((copilot copilot-no-mode-indent))
+     ((copilot copilot-no-mode-indent))
+     ((copilot copilot-no-mode-indent))
+     ((copilot copilot-no-mode-indent))
+     (org-roam)
+     (org-roam)
+     (org-roam)
+     (org-element-cache)
+     (org-element-cache)
+     (defvaralias)
+     (org-mode-hook))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
